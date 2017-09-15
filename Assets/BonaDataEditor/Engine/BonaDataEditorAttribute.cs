abc79e186a63a0dc236cc1245a43e8d377f6eea1
Bona <bjorn.fyrvall@gmail.com>
2017-09-15 14:09:31 +0200

    Initial release.

---
+++ Assets/BonaDataEditor/Engine/BonaDataEditorAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BonaDataEditorAttribute : Attribute
+{
+    public string DisplayName = string.Empty;
+    public int SortOrder = int.MaxValue;
+}
